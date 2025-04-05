c85d9a198081d1ff597147170d9ef29180877560
MAHENDRA4593 <madan452218@gmail.com>
2025-04-05 16:30:37 +0530

    Update n-virginia_ec2.tf

--- n-virginia_ec2.tf
+++ n-virginia_ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "mahe" {
 count      =  5
   ami           = "ami-00a929b66ed6e0de6"
   instance_type = "t2.micro"
-  security_group_id = "sg-0deb3ce7556848dbc"
+  security_group_id = ["sg-0deb3ce7556848dbc"]
 
   tags = {
     Name = "rohit"
